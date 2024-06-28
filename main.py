809f7f6dc60a78833dcdc87d603884bbab70e6d2
Manuel Aranguri <67668320+Nwyr-34@users.noreply.github.com>
2024-06-28 22:21:27 +0000

    endpoint desarrollado
    endpoint para calcular la formula de carga axial critica

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hola mundo")
+from fastapi import FastAPI
+from pydantic import BaseModel
+import math
+
+app = FastAPI()
+
+
+class InputData(BaseModel):
+  E: float
+  I: float
+  L: float
+
+
+@app.post("/calculate/")
+async def calculate(data: InputData):
+  P_cr = (math.pi**2 * data.E * data.I) / (data.L**2)
+  return {"Resultado": P_cr}
