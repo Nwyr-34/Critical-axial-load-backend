0dd23734bd91add1e3424494a197740bffa8b91c
Manuel Aranguri <67668320+Nwyr-34@users.noreply.github.com>
2024-06-29 01:11:24 +0000

    problemas de CORS arreglado

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import math
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace "*" with the specific origin of your frontend
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 
 class InputData(BaseModel):
